b8868cd5714f9ecd555de70db3dd1a7a78fcd37b
yezi <yezi@grinnell.edu>
2023-10-14 06:05:49 -0500

    Create AACCategory and AACMappings classes

---
+++ src/AACCategory.java
@@ -0,0 +1,46 @@
+/**
+ * A basic implementation of ....
+ *
+ * @author Lydia Ye
+ */
+
+public class AACCategory {
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The key.
+   */
+
+
+  /**
+   * The value.
+   */
+
+
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  /**
+   * Create a new empty category with the given name
+   */
+  public AACCategory(String name) {
+    //STUB
+  } // AACCategory()
+
+  // +------------------+--------------------------------------------
+  // | Standard methods |
+  // +------------------+
+
+  /*
+   * Adds the mapping of the imageLoc to the test to the category.
+   */
+  public void addItem(String imageLoc, String text) {
+    return; //STUB
+  } // addItem(String, String)
+
+
+} // class AACCategory
+
